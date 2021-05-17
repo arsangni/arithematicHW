aa5c5c12e98e0515c5511ffb38c767d29479abc4
arsangni <arsangni@gmail.com>
2021-05-17 13:15:13 +0530

    second commit

---
+++ ArithematicHW.R
@@ -0,0 +1,68 @@
+num0 = 759
+num1 = 123
+num1 + num0
+num0 - num1
+num1 * num0
+num1 / num0
+num1< num0
+num1 > num0
+num11=num0
+num11
+z=4
+x=z
+1+6
+sqrt(12)
+#exponents
+10^6
+11^5
+38*9
+40000/20
+?sqrt
+?mean
+sqrt(46)
+mean(475,7577,300)
+mean(c(475,7577,300))
+z<- c(475,7577,300)
+mean (z)
+sum(z)
+max(z)
+min(z)
+z=18
+z
+str(z)
+class(z)
+x=20
+str(x)
+class(x)
+x= "heyyy"
+x
+str(x)
+class(x)
+z= "xyz123"
+z
+class(z)
+str(z)
+z<- 76:80
+z
+z<- 41:50
+z
+str(z)
+class(z)
+n<- c(0.0,698,877,74.78,30:49)
+n
+n[c(2,3)]
+as.integer (n)
+round(n)
+j<-c("1.8777","2.432","59.5")
+sum(as.numeric(j))
+round(as.numeric(j))
+is.numeric(j)
+is.character(j)
+n <- as.numeric(j)
+is.numeric(n)
+numeric()
+x <- c(168,0.0009,3.0098,900,4:9)
+str(x)
+class(x)
+x[c(3,4)]
+
